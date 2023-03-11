666c049e66fe448950f40d02d3ef704e28c656ff
KevinEsh <52937054+KevinEsh@users.noreply.github.com>
2023-03-11 13:51:07 -0600

    lorentzian classifier almost ready

--- lorentzian_classifier.py
+++ lorentzian_classifier.py
@@ -1,9 +1,8 @@
 from sklearn.neighbors import KNeighborsClassifier
 import pandas as pd
 import numpy as np
-from ta.momentum import RSIIndicator
-from ta.trend import ADXIndicator, CCIIndicator
 from enum import Enum
+from typing import Any
 
 
 class TradeSignal(Enum):
@@ -18,24 +17,53 @@ class LorentzianClassifier:
     ML model, this value is hardcoded to 4 bars but support for other training
     lengths may be added in the future.
     """
-    pass
 
+    def __init__(
+        self,
+        n_neighbors: int = 4,
+        lookback_window: int = 2000,
+        neighbors_leap: int = 4,
+        n_jobs: int = 1
+    ) -> None:
+        # define the number of neighbors to use
+        self._n_neighbors = n_neighbors
+        self._lookback_window = lookback_window
+        self._neighbors_leap = neighbors_leap
+        self._n_jobs = n_jobs
 
-def get_modif_lorentzian_distance(x1: np.ndarray, x2: np.ndarray, lookback_window: float = 2000) -> float:
-    i1, i2 = x1[0], x2[0]  # get the chronological index of both points
-    is_not_in_window = (i1 < i2) and (i1 <= i2 + lookback_window)
-    is_not_module = not (i2 - lookback_window) % 4  # TODO: cambiar esta cosa por 4 velas atras de i2 (experimentar)
-    if is_not_module or is_not_in_window:
-        return np.inf
-    return np.sum(np.log(1 + np.abs(x1[1:] - x2[1:])))
+        # create the KNN classifier using the Lorentzian distance metric
+        self.knn_classifier = KNeighborsClassifier(
+            n_neighbors=self._n_neighbors,
+            n_jobs=self._n_jobs,
+            metric=self._modif_lorentzian_distance,
+            weights=self._distance_weights
+            # metric_params={"lookback_window": 2000},
+        )
 
+    def fit(self, train_data: Any, train_target: Any) -> None:
+        # fit the model
+        self._train_data = train_data
+        self._train_target = train_target
+        self.knn_classifier.fit(self._train_data, self._train_target)
 
-def normalized_weights(distances: np.ndarray) -> np.ndarray:
-    # convert inf distance to 0, they don't have any importance
-    is_inf = np.isinf(distances)
-    distances[is_inf] = 0
-    # normalize weigths using their distance. Further points will have greater importance
-    return distances / np.sum(distances, axis=1, keepdims=True)
+    def predict(self, data: Any) -> Any:
+        return self.knn_classifier.predict(data)
+
+    def _modif_lorentzian_distance(self, x1: np.ndarray, x2: np.ndarray) -> float:
+        i1, i2 = x1[0], x2[0]  # get the chronological index of both points
+        is_not_in_window = (i1 < i2) and (i1 <= i2 + self._lookback_window)
+        # TODO: cambiar esta cosa por 4 velas atras de i2 (experimentar)
+        skip_neighbor = not (i1 - i2) % self._neighbors_leap
+        if skip_neighbor or is_not_in_window:
+            return np.inf
+        return np.sum(np.log(1 + np.abs(x1[1:] - x2[1:])))
+
+    def _distance_weights(self, distances: np.ndarray) -> np.ndarray:
+        # convert inf distance to 0, they will be disregarded
+        is_inf = np.isinf(distances)
+        distances[is_inf] = 0
+        # normalize weigths using their distance. Further points will have less impact
+        return distances / np.sum(distances, axis=1, keepdims=True)
 
 
 X = pd.DataFrame({'feature1': [1, 2, 3, 4, 5],
@@ -44,29 +72,11 @@ X = pd.DataFrame({'feature1': [1, 2, 3, 4, 5],
 
 y = pd.Series([0, 0, 0, -1, -1])
 
-RSIIndicator(X["feature1"])
-
-
-# define the number of neighbors to use
-n_neighbors = 2
-
-# create the KNN classifier using the Lorentzian distance metric
-knn = KNeighborsClassifier(
-    n_neighbors=n_neighbors,
-    n_jobs=2,
-    metric_params={"lookback_window": 2},
-    metric=get_modif_lorentzian_distance,
-    weights=normalized_weights
-)
-
-# fit the model
-knn.fit(X, y)
 
 # make some predictions on new data
 X_new = pd.DataFrame({'feature1': [6, 7, 8, 1],
                       'feature2': [12, 14, 16, 3],
                      'feature3': [15, 17, 19, 100]})
 
-y_pred = knn.predict(X_new)
 
 print(y_pred)
