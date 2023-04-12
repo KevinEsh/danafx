ee28991ba7e56999b9101e6d7077b555b1905ad4
KevinEsh <52937054+KevinEsh@users.noreply.github.com>
2023-04-12 10:59:18 -0600

    trader bot

--- traderbot.py
+++ traderbot.py
@@ -1,35 +1,44 @@
-from broker import BrokerSession
-from state_machine import AssetStateMachine
 from time import sleep
 from logging import log
 
+from trade.broker import BrokerSession
+from trade.state_machine import AssetStateMachine
+from trade.strategies import MovingAverageStrategy
 
-class TradingStrategy:
-    def __init__(self) -> None:
-        pass
-        self.price = 1.08105
-        self.position = None
+# class TradingStrategy:
+#     def __init__(self) -> None:
+#         pass
+#         self.price = 1.08105
+#         self.position = None
 
-    def get_entry_signal(self, data):
-        if data.close > self.price:
-            return 1
-        # if data.close < self.price:
-        #     return -1
-        else:
-            return 0
+#     def get_entry_signal(self, data):
+#         if data.close > self.price:
+#             return 1
+#         # if data.close < self.price:
+#         #     return -1
+#         else:
+#             return 0
 
-    def get_exit_signal(self, data):
-        if data.close >= self.price + 0.00001 * 5:
-            return 1
-        # elif data.close <= self.price - 0.0001 * 2:
-        #     return 1
-        else:
-            return 0
+#     def get_exit_signal(self, data):
+#         if data.close >= self.price + 0.00001 * 5:
+#             return 1
+#         # elif data.close <= self.price - 0.0001 * 2:
+#         #     return 1
+#         else:
+#             return 0
 
 
 class SingleTraderBot:
 
-    def __init__(self, login_settings) -> None:
+    def __init__(
+        self,
+        symbol: str,
+        broker: BrokerSession,
+        timeframe: str,
+        strategy: str,
+        leap_in_secs: float,
+        risk_params: dict,
+    ) -> None:
         from setup import get_settings
 
         login_settings = get_settings("settings/demo/login.json")
@@ -45,12 +54,15 @@ class SingleTraderBot:
         self.state = AssetStateMachine(symbol)
         self.symbol = symbol
         self.timeframe = "M1"
-        self.open_positions = None
         self.leap_in_secs = 5
-        self.position = None
-        self.strategy = TradingStrategy()
+        self.strategy = MovingAverageStrategy(20, 200)
+        self.risk_params = {
+            "pips": 20,
+            "risk_tolerance": 0.02,
+            "rr_ratio": 2,
+        }
 
-    def add_strategy(self, strategy: TradingStrategy) -> None:
+    def add_strategy(self, strategy) -> None:
         pass
 
     def run(self) -> None:
@@ -69,44 +81,43 @@ class SingleTraderBot:
             #     number_of_ticks=1,
             #     format_time=False
             # )[-1]
-            latest_candle = self.broker.get_candles(
-                symbol=self.symbol,
-                timeframe=self.timeframe,
-                n_candles=1,
-            )[-1]
             # print(latest_tick)
-            print(latest_candle.close)
+            candles = self.broker.get_candles(self.symbol, self.timeframe, 2)
+            last_candles, current_candle = candles[:-1], candles[-1]
+            print(current_candle.close)
+
+            # Always update data to save computational time and memory
+            self.strategy.update_data(last_candles)
 
             if self.state.no_position:
 
                 # If no position is placed, create an entry signal, this signal could be buy, sell or neutral
-                entry_signal = self.strategy.get_entry_signal(latest_candle)
+                entry_signal = self.strategy.get_entry_signal(current_candle)
+                print(entry_signal)
 
                 if self.state.is_entry(entry_signal):
-                    print(f"Creating order {self.symbol}")
-                    order_type = "sell" if entry_signal == -1 else "buy"  # is_buy_or_sell(entry_signal)
-                    order_params = self.broker.calc_risk_params(
-                        self.symbol, order_type, pips=10, risk_tolerance=0.02, rr_ratio=2)
-                    print(order_params)
+                    print(f"Creating {entry_signal.name} order for {self.symbol}")
+                    order_type = entry_signal.name.lower()  # TODO
+                    order_params = self.broker.calc_risk_params(self.symbol, order_type, **self.risk_params)
                     self.order = self.broker.create_order(self.symbol, order_type, *order_params)
                     self.state.transition(entry_signal)
                     print(f"Order created for {self.symbol}")
 
-            elif self.state.awaiting_position:
+            if self.state.awaiting_position:
 
-                self.position = self.broker.get_positions(self.symbol)[-1]
-                print(f"Position {self.position.ticket} placed for {self.symbol}")
+                self.strategy.position = self.broker.get_positions(self.symbol)[-1]
+                print(f"Position {self.strategy.position.ticket} placed for {self.symbol}")
                 self.state.transition(entry_signal)
 
-            elif self.state.on_long or self.state.on_short:
+            if self.state.on_long or self.state.on_short:
 
-                exit_signal = self.strategy.get_exit_signal(latest_candle)
+                exit_signal = self.strategy.get_exit_signal(current_candle)
 
                 if self.state.is_exit(exit_signal):
-                    exit_order = self.broker.close_position(self.position, order_type)
-                    # self.position = self.broker.get_positions(exit_order)
+                    self.broker.close_position(self.strategy.position, order_type)
+                    self.strategy.position = None
                     self.state.transition(exit_signal)
-                    print(f"Position {self.position.ticket} closed for {self.symbol}")
+                    print(f"Position {self.strategy.position.ticket} closed for {self.symbol}")
 
                 else:
                     # strategy.update_trailing_stop(order=position, trailing_stop_pips=10, pip_size=trading_settings['pip_size'])
