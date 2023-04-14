9216110f6b76f8711918b017b06711cdc23dd35a
KevinEsh <52937054+KevinEsh@users.noreply.github.com>
2023-04-14 15:44:09 -0600

    new logic on state_machine

--- trade/state_machine.py
+++ trade/state_machine.py
@@ -6,44 +6,20 @@ class AssetStateMachine:
     """Define the state machine and its transitions
     """
 
-    def __init__(self, symbol: str, init_state: AssetState = AssetState.NULL_POSITION) -> None:
-        self.symbol = symbol
+    def __init__(self, init_state: AssetState = AssetState.NULL_POSITION) -> None:
         self._state = init_state
 
-    def transition(self, signal: Union[EntrySignal, ExitSignal]):
-        # Looking for buy/sell entries when there is no positions
+    def next(self):
+        # looking for buy/sell entries when there is no positions
         if self._state == AssetState.NULL_POSITION:
-
-            if signal == EntrySignal.NEUTRAL:
-                return
-            if signal in [EntrySignal.BUY, EntrySignal.SELL]:
-                self._state = AssetState.WAITING_POSITION
-                return
-            else:
-                raise ValueError(f"Entry {signal=} not recognized")
-
+            self._state = AssetState.WAITING_POSITION
+        # An entry was found, wait until the order is taken by the broker
         elif self._state == AssetState.WAITING_POSITION:
-            if signal == EntrySignal.BUY:
-                self._state = AssetState.LONG_POSITION
-                return
-            elif signal == EntrySignal.SELL:
-                self._state = AssetState.SHORT_POSITION
-                return
-            elif signal == EntrySignal.NEUTRAL:
-                return
-            else:
-                raise ValueError(f"Entry {signal=} not recognized")
-
-        # Looking for buy/sell entries when there is no positions
-        elif self._state in [AssetState.LONG_POSITION, AssetState.SHORT_POSITION]:
-            # Strategic exit is raised by touching a Take-Profit
-            if signal == ExitSignal.EXIT:
-                self._state = AssetState.NULL_POSITION
-                return
-            elif signal == ExitSignal.HOLD:
-                return
-            else:
-                raise ValueError(f"Exit {signal=} not recognized")
+            self._state = AssetState.ON_POSITION
+        # looking for buy/sell exits when there is a position placed
+        elif self._state == AssetState.ON_POSITION:
+            self._state = AssetState.NULL_POSITION
+        return
 
     def is_entry(self, signal: EntrySignal) -> bool:
         if signal in [EntrySignal.BUY, EntrySignal.SELL]:
@@ -56,7 +32,7 @@ class AssetStateMachine:
         return False
 
     @ property
-    def no_position(self) -> bool:
+    def null_position(self) -> bool:
         return self._state == AssetState.NULL_POSITION
 
     @ property
@@ -64,12 +40,8 @@ class AssetStateMachine:
         return self._state == AssetState.WAITING_POSITION
 
     @ property
-    def on_long(self) -> bool:
-        return self._state == AssetState.LONG_POSITION
-
-    @ property
-    def on_short(self) -> bool:
-        return self._state == AssetState.SHORT_POSITION
+    def on_position(self) -> bool:
+        return self._state == AssetState.ON_POSITION
 
 
 if __name__ == "__main__":
