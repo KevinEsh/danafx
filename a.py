48370e2673098cde041b6b4c17703c53908cadda
KevinEsh <52937054+KevinEsh@users.noreply.github.com>
2023-04-18 18:33:16 -0600

    new features from plotly

--- a.py
+++ a.py
@@ -1,4 +1,4 @@
-from tools.config import get_settings
+from utils.config import get_settings
 from trade.broker import BrokerSession
 import plotly.graph_objects as go
 from plotly.subplots import make_subplots
@@ -16,6 +16,7 @@ broker.start_session(login_settings["mt5_login"])
 broker.enable_symbols([symbol])
 
 df = broker.get_candles(symbol, "M1", 100, as_dataframe=True, format_time=True)
+# df.to_csv("eurusd.csv")
 
 # fig = go.Figure(data=go.Ohlc(x=df.index,
 #                              open=df.open,
@@ -31,9 +32,11 @@ fig.add_trace(go.Ohlc(x=df.index,
 
 fig.update_layout(xaxis_rangeslider_visible=False, title=symbol)
 # fig.update_xaxes(title_text='Date')
-fig.update_yaxes(title_text='price', tickprefix='$', ticklabelposition="inside top", row=1, col=1)
+fig.update_yaxes(ticklabelposition="outside right", side="right")
+fig.update_yaxes(title_text='price', tickprefix='$', row=1, col=1)
+fig.update_yaxes(title_text='RSI(14)', row=2, col=1)
 fig.update_xaxes(showline=True, linewidth=2, linecolor='black')
-fig.add_trace(go.Scatter(y=df.close, mode="lines"), row=2, col=1)
+fig.add_trace(go.Scatter(x=df.index, y=df.close, mode="lines"), row=2, col=1)
 
 x = df.index[50:60]
 y = df.close[50:60]
