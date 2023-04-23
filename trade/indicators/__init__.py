e8bee4a873076f315ef14de8a67bc9cc76fc87dd
KevinEsh <52937054+KevinEsh@users.noreply.github.com>
2023-04-23 17:36:57 -0600

    move dir

--- trade/indicators/__init__.py
+++ trade/indicators/__init__.py
@@ -1,5 +1,5 @@
 from talib import EMA, SMA, CCI, ADX, RSI, MAX, MIN
-from new_indicators import HL2, HLC3, OHLC4, DONCHAIN  # TODO, RQK, RBFK
+from custom import HL2, HLC3, OHLC4, DONCHAIN  # TODO, RQK, RBFK
 
 from talib import set_unstable_period, get_unstable_period
 from basic import get_all_indicators, get_min_bars, get_stable_min_bars
