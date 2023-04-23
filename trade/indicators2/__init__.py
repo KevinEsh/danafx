b825824aa1b3d7066f33ada33be42f553ce7393b
KevinEsh <52937054+KevinEsh@users.noreply.github.com>
2023-04-23 17:03:40 -0600

    init file indicators2

---
+++ trade/indicators2/__init__.py
@@ -0,0 +1,5 @@
+from talib import EMA, SMA, CCI, ADX, RSI, MAX, MIN
+from new_indicators import HL2, HLC3, OHLC4, DONCHAIN  # TODO, RQK, RBFK
+
+from talib import set_unstable_period, get_unstable_period
+from basic import get_all_indicators, get_min_bars, get_stable_min_bars
