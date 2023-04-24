2ba802c7d0c34347e88609376521315c91683305
KevinEsh <52937054+KevinEsh@users.noreply.github.com>
2023-04-24 01:15:35 -0600

    implement dash in dashboard

--- a.py
+++ a.py
@@ -9,6 +9,7 @@
 # broker = BrokerSession()
 # broker.start_session(login_settings["mt5_login"])
 # broker.enable_symbols([symbol])
+from dash import Dash, dcc, html, Input, Output
 from pandas import read_csv
 from yfinance import download
 from datetime import datetime as dt
@@ -18,13 +19,13 @@ import plotly.graph_objects as go
 from plotly.subplots import make_subplots
 from trade.indicators import RSI, get_stable_min_bars, set_unstable_period
 
-symbol = "EURUSD"
-tz = pytz.timezone("US/Central")
-start = tz.localize(dt(2023, 4, 1))
-end = tz.localize(dt.today())
+symbols = ["EURUSD"]
+# tz = pytz.timezone("US/Central")
+# start = tz.localize(dt(2023, 4, 1))
+# end = tz.localize(dt.today())
 
 # df = broker.get_candles(symbol, "M1", 100, as_dataframe=True, format_time=True)
-df = read_csv("data/raw/eurusd_2.csv", index_col="time")
+df = read_csv("data/raw/eurusd_365.csv", index_col="time")
 # df = download("EURUSD=X", start, end, interval="1h", auto_adjust=True)
 # df.rename(columns={C:C.lower() for C in df.columns}, inplace=True)
 # df.to_csv("data/raw/eurusd_2.csv")
@@ -34,41 +35,26 @@ bars = get_stable_min_bars("RSI", 10)
 set_unstable_period("RSI", bars - 10 + 1)
 rsi2 = RSI(df.close, 10)
 
-fig = make_subplots(rows=2, cols=1)
-# df.to_csv("eurusd.csv")
-
-# fig = go.Figure(data=go.Ohlc(x=df.index,
-#                              open=df.open,
-#                              high=df.high,
-#                              low=df.low,
-#                              close=df.close))
-
-fig.add_trace(go.Ohlc(x=df.index,
-                      open=df.open,
-                      high=df.high,
-                      low=df.low,
-                      close=df.close,
-                       name="price",
-                      ), row=1, col=1)
-
-fig.update_layout(xaxis_rangeslider_visible=False, title=symbol, legend=dict(groupclick="toggleitem", orientation="h", xanchor="auto", yanchor="auto", y=-0.12))
-# fig.update_xaxes(title_text='Date')
-fig.update_yaxes(ticklabelposition="outside right", side="right")
-fig.update_yaxes(tickprefix='$', row=1, col=1)
-fig.update_yaxes(range=(0, 100), fixedrange=True, row=2, col=1)
-fig.update_xaxes(showline=True, linewidth=2, linecolor='black')
-
-
-fig.add_trace(go.Scatter(x=df.index, y=rsi, mode="lines", name="RSI(14)", legendgroup="indicators"), row=2, col=1)
-fig.add_trace(go.Scatter(x=df.index, y=rsi2, mode="lines", name="RSI2(14)", legendgroup="indicators"), row=2, col=1)
-
 x = df.index[50:60]
 y = df.close[50:60]
 
 x2 = df.index[60:70]
 y2 = df.close[60:70]
 
-fig.add_trace(
+fig = make_subplots(rows=2, cols=1)
+# df.to_csv("eurusd.csv")
+
+fig_ohlc = go.Figure(
+    data=go.Ohlc(
+        x=df.index,
+        open=df.open,
+        high=df.high,
+        low=df.low,
+        close=df.close,
+        name="price"
+    )
+)
+fig_ohlc.add_trace(
     go.Scatter(
         x=x,
         y=y,
@@ -77,21 +63,20 @@ fig.add_trace(
         mode='markers',
         opacity=0.8,
         marker=dict(
-            size=12, 
-            symbol="arrow", 
-            angle=45, 
-            color="Green", 
+            size=12,
+            symbol="arrow",
+            angle=45,
+            color="Green",
             # line=dict(
-            #     width=2, 
+            #     width=2,
             #     color="Green"
             #     )
         ),
     ),
-    row=1,
-    col=1,
+    # row=1,
+    # col=1,
 )
-
-fig.add_trace(
+fig_ohlc.add_trace(
     go.Scatter(
         x=x2,
         y=y2,
@@ -100,18 +85,131 @@ fig.add_trace(
         mode='markers',
         opacity=0.8,
         marker=dict(
-            size=12, 
-            symbol="arrow", 
-            angle=135, 
-            color="Red", 
+            size=12,
+            symbol="arrow",
+            angle=135,
+            color="Red",
             # line=dict(
-            #     width=2, 
+            #     width=2,
             #     color="Red"
             # )
         ),
     ),
-    row=1,
-    col=1,
+    # row=1,
+    # col=1,
+)
+fig_ohlc.update_layout(
+    xaxis_rangeslider_visible=False,
+    legend=dict(
+        groupclick="toggleitem",
+        orientation="h",
+        # xanchor="left",
+        # yanchor="top",
+        y=1.2,
+        x=0,
+    )
+)
+fig_ohlc.update_yaxes(
+    ticklabelposition="outside right",
+    tickprefix='$',
+    side="right"
+)
+# fig_ohlc.update_yaxes()
+
+# fig.add_trace(go.Ohlc(x=df.index,
+#                       open=df.open,
+#                       high=df.high,
+#                       low=df.low,
+#                       close=df.close,
+#                       name="price",
+#                       ), row=1, col=1)
+
+# fig.update_layout(xaxis_rangeslider_visible=False, legend=dict(
+#     groupclick="toggleitem", orientation="h", xanchor="auto", yanchor="auto", y=-0.12))
+# fig.update_xaxes(title_text='Date')
+# fig.update_yaxes(ticklabelposition="outside right", side="right")
+# fig.update_yaxes(tickprefix='$', row=1, col=1)
+# fig.update_yaxes(range=(0, 100), fixedrange=True, row=2, col=1)
+# fig.update_xaxes(showline=True, linewidth=2, linecolor='black')
+
+fig_indic = go.Figure()
+fig_indic.add_trace(
+    go.Scatter(
+        x=df.index,
+        y=rsi,
+        mode="lines",
+        name="RSI(14)",
+        legendgroup="indicators"
+    )
 )
 
-fig.show()
+fig_indic.add_trace(
+    go.Scatter(
+        x=df.index,
+        y=rsi2,
+        mode="lines",
+        name="RSI2(14)",
+        legendgroup="indicators",
+    )
+)
+
+fig_indic.update_yaxes(
+    side="right",
+    ticklabelposition="outside right",
+    fixedrange=True,
+    range=(0, 100),
+)
+fig_indic.update_layout(
+    # xaxis_rangeslider_visible=False,
+    legend=dict(
+        groupclick="toggleitem",
+        orientation="h",
+        # xanchor="left",
+        # yanchor="top",
+        y=1.2,
+        x=0,
+    )
+)
+
+app = Dash(__name__)
+
+app.layout = html.Div([
+    html.Div([
+        dcc.Dropdown(
+            options=symbols,
+            value=symbols[0],
+            id='symbol-selector-dropdown'
+        )],
+        style={'width': '15%', 'display': 'inline-block'}
+    ),
+    dcc.Graph(id='candlestick-graph'),
+    dcc.Graph(id='indicators-graph'),
+    # dcc.Slider(
+    #     df['year'].min(),
+    #     df['year'].max(),
+    #     step=None,
+    #     value=df['year'].min(),
+    #     marks={str(year): str(year) for year in df['year'].unique()},
+    #     id='year-slider'
+    # )
+])
+
+
+@app.callback(
+    Output('candlestick-graph', 'figure'),
+    Output('indicators-graph', 'figure'),
+    Input('symbol-selector-dropdown', 'value'),
+    # Output('indicators-graph', 'figure')
+)
+def update_symbol(symbol):
+    # filtered_df = df[df.year == symbol]
+
+    # fig = px.scatter(filtered_df, x="gdpPercap", y="lifeExp",
+    #                  size="pop", color="continent", hover_name="country",
+    #                  log_x=True, size_max=55)
+
+    return fig_ohlc, fig_indic
+
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
