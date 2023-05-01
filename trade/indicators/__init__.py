f3e2bb2f09dadc94ac021ef5eceb7c32749f89f5
KevinEsh <52937054+KevinEsh@users.noreply.github.com>
2023-04-30 23:36:54 -0600

    WT included

--- trade/indicators/__init__.py
+++ trade/indicators/__init__.py
@@ -1,5 +1,5 @@
 from talib import EMA, SMA, CCI, ADX, RSI, MAX, MIN
-from trade.indicators.custom import HL2, HLC3, OHLC4, DONCHAIN  # TODO, RQK, RBFK
+from trade.indicators.custom import HL2, HLC3, OHLC4, DONCHAIN, WT  # TODO, RQK, RBFK
 
 from talib import set_unstable_period, get_unstable_period
 from trade.indicators.basic import get_all_indicators, get_min_bars, get_stable_min_bars
