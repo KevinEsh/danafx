ce4d066dc157c1e5f42ee4b2e47afcfc88f6559d
KevinEsh <52937054+KevinEsh@users.noreply.github.com>
2023-05-11 20:49:36 -0600

    crossing functions

--- datatools/technical.py
+++ datatools/technical.py
@@ -1,45 +1,53 @@
-from numpy import ndarray
+import numpy as np
 
 
-def crossingover(
-    source1: ndarray,
-    source2: ndarray,
-    delta: float = 0
-) -> bool:
-    """Check if the final value of source1 is greater than the final value of source2
-    #and if the value of source1 on the previous bar was less than or equal to
-    the value of source2
+def crossingover(arr1: np.ndarray, arr2: np.ndarray, delta: float = 0):
+    """Check if the value of arr2 is greater than the final value of arr2
+    #and if the value of arr1 on the previous bar was less than or equal to
+    the value of arr2
 
     Args:
-        source1 (ndarray): Source data that has to cross over from bottom to top
-        source2 (ndarray): Source data to test crossing condition
+        arr1 (ndarray): Source data that has to cross over from bottom to top
+        arr2 (ndarray): Source data to test crossing condition
 
     Returns:
-        bool: Signal if the source1 crosses over source2
+        bool: Signal if the arr1 crosses over arr2
     """
-    if (source1[-1] > source2[-1] + delta) and (source1[-2] <= source2[-2]):
-        return True
-    else:
-        return False
+    # Find the points where arr1 crosses over arr2
+    cross_points = np.logical_and(
+        arr1[:-1] <= arr2[:-1] + delta,  # below condition
+        arr1[1:] > arr2[1:] + delta)  # over condition
+
+    # Create a boolean array of the same shape as the input arrays
+    result = np.zeros_like(arr1, dtype=bool)
+
+    # Set the values of the result array to True where arr1 crosses over arr2
+    result[1:][cross_points] = True
+
+    return result
 
 
-def crossingunder(
-    source1: ndarray,
-    source2: ndarray,
-    delta: float = 0,
-) -> bool:
-    """Check if the final value of source1 is greater than the final value of source2
-    #and if the value of source1 on the previous bar was less than or equal to
-    the value of source2
+def crossingunder(arr1: np.ndarray, arr2: np.ndarray, delta: float = 0):
+    """Check if the value of arr1 is greater than the final value of arr2
+    #and if the value of arr1 on the previous bar was less than or equal to
+    the value of arr2
 
     Args:
-        source1 (ndarray): Source data that has to cross over from bottom to top
-        source2 (ndarray): Source data to test crossing condition
+        arr1 (ndarray): Source data that has to cross over from bottom to top
+        arr2 (ndarray): Source data to test crossing condition
 
     Returns:
-        bool: Signal if the source1 crosses under source2
+        bool: True if the arr1 crosses under arr2
     """
-    if (source1[-1] < source2[-1] + delta) and (source1[-2] >= source2[-2]):
-        return True
-    else:
-        return False
+    # Find the points where arr1 crosses over arr2
+    cross_points = np.logical_and(
+        arr1[:-1] >= arr2[:-1] + delta,  # upper condition
+        arr1[1:] < arr2[1:] + delta)  # under condition
+
+    # Create a boolean array of the same shape as the input arrays
+    result = np.zeros_like(arr1, dtype=bool)
+
+    # Set the values of the result array to True where arr1 crosses over arr2
+    result[1:][cross_points] = True
+
+    return result
