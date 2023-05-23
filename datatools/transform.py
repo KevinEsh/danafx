b1f9d783b33cb5c0710ff54866cf9afec8ef697f
KevinEsh <52937054+KevinEsh@users.noreply.github.com>
2023-05-22 18:56:12 -0600

    new way to create FunctionTranformator

--- datatools/transform.py
+++ datatools/transform.py
@@ -1,16 +1,50 @@
-from sklearn.preprocessing import minmax_scale
 from talib import EMA, SMA
-
-from trade.metadata import CandleLike
-
-from sklearn.pipeline import Pipeline
-from sklearn.preprocessing import StandardScaler, MinMaxScaler
-from sklearn.impute import SimpleImputer
-from sklearn.base import BaseEstimator, TransformerMixin
 from scipy.signal import savgol_filter
-import numpy as np
-from trade.strategies.abstract import Hyperparameter
-# Define a custom transformer to apply a Savitzky-Golay filter to the data
+from sklearn.preprocessing import StandardScaler, MinMaxScaler, FunctionTransformer
+from sklearn.base import BaseEstimator, TransformerMixin
+
+
+def range_scale(X, feature_range: tuple[int, int] = (0, 100)):
+    return (X - feature_range[0]) / (feature_range[1] - feature_range[0])
+
+
+def smooth(X, window: int, method: str = "SMA"):
+    if window < 1:
+        raise ValueError(f"{window=} should be greater than 0")
+    if window == 1:
+        return X
+    elif method == "SMA":
+        return SMA(X, window)
+    elif method == "EMA":
+        # TODO: min_bars = get_stable_min_bars("EMA", window_smooth)
+        # EMA.set
+        return EMA(X, window)
+
+
+def get_smoother(
+    method: str,
+    **kwargs: int,
+):
+    if method == "EMA":
+        return FunctionTransformer(EMA, kw_args=kwargs)
+    elif method == "SMA":
+        return FunctionTransformer(SMA, kw_args=kwargs)
+    elif method == "savitz":
+        return FunctionTransformer(savgol_filter, kw_args=kwargs)
+
+
+def get_scaler(
+    method: str,
+    **kwargs
+):
+    if method == "range_scale":
+        return FunctionTransformer(range_scale, kw_args=kwargs)
+    elif method == "minmax_scale":
+        return MinMaxScaler(**kwargs)
+    elif method == "standard_scale":
+        return StandardScaler(**kwargs)
+
+
 class RangeScaler(BaseEstimator, TransformerMixin):
     config_feature_range = Hyperparameter("feature_range", "interval", (-1000, 1000))
     # config_window = Hyperparameter("window", "numeric", (1, 100))
@@ -20,13 +54,14 @@ class RangeScaler(BaseEstimator, TransformerMixin):
 
         self.feature_range = feature_range
         self.copy = copy
-        
+
     def fit(self, X, y=None):
         return self
 
     def transform(self, X, y=None):
         return (X - self.feature_range[0]) / (self.feature_range[1] - self.feature_range[0])
         # return savgol_filter(X, self.window_length, self.polyorder, axis=0)
+
 
 class Smoother(BaseEstimator, TransformerMixin):
     config_window = Hyperparameter("window", "numeric", (1, 100))
@@ -40,10 +75,10 @@ class Smoother(BaseEstimator, TransformerMixin):
         self.window = window
         self.method = method
         self.copy = copy
-        
+
     def fit(self, X, y=None):
         return self
-    
+
     def transform(self, X, y=None):
         if self.window == 1:
             return X
@@ -55,41 +90,15 @@ class Smoother(BaseEstimator, TransformerMixin):
             return EMA(X, self.window)
 
 # Define a custom transformer to apply a Savitzky-Golay filter to the data
+
+
 class SavitzkyGolayFilter(BaseEstimator, TransformerMixin):
     def __init__(self, window=5, polyorder=2):
         self.window = window
         self.polyorder = polyorder
-        
+
     def fit(self, X, y=None):
         return self
-    
+
     def transform(self, X, y=None):
         return savgol_filter(X, self.window, self.polyorder, axis=0)
-
-
-def normalize(
-    data: CandleLike,
-    xrange: tuple[float, float] = (0, 1),
-    method: str = "minmax_scale"
-) -> CandleLike:
-    if method == "range_scale":
-        return (data - xrange[0]) / (xrange[1] - xrange[0])
-    elif method == "minmax_scale":
-        return minmax_scale(data, xrange, copy=False)
-
-
-def smooth(
-    data: CandleLike,
-    window_smooth: int = 2,
-    method: str = "EMA",
-) -> CandleLike:
-    if window_smooth < 1:
-        raise ValueError(f"{window_smooth=} should be greater than 0")
-    elif window_smooth == 1:
-        return data
-    elif method == "SMA":
-        return SMA(data, window_smooth)
-    elif method == "EMA":
-        # TODO: min_bars = get_stable_min_bars("EMA", window_smooth)
-        # EMA.set
-        return EMA(data, window_smooth)
