90eded32d8a5e59fa20c687056b77fafeb33eca2
KevinEsh <52937054+KevinEsh@users.noreply.github.com>
2023-05-22 18:55:16 -0600

    plotting functions

--- utils/plotting.py
+++ utils/plotting.py
@@ -1,75 +1,128 @@
-import pandas as pd
+import numpy as np
 import plotly.graph_objects as go
-import mplfinance as fplt
-from numpy import nan
+
+from trade.metadata import CandleLike
+from datatools.technical import above, below, onband
 
 
-def plot_candlestick_chart(df_rates, symbol: str, timestamp: str) -> None:
-    # change columns names to make df compatible with mplfinance
-    mpl_comp_names = {
-        'time': 'Date',
-        'open': 'Open',
-        'high': 'High',
-        'low': 'Low',
-        'close': 'Close',
-        'tick_volume': 'Volume'
-    }
-    saving_config = {
-        "fname": f'{symbol}_{timestamp}.jpeg',
-        # "dpi": None,
-        # "pad_inches": 0.01,
-        # "transparent": True
-    }
+def make_figure():
+    return go.Figure()
 
-    uptrend = df_rates.regression * df_rates.signal.apply(lambda s: nan if s != 1 else 1)
-    downtrend = df_rates.regression * df_rates.signal.apply(lambda s: nan if s != -1 else 1)
 
-    trendmarkers = [
-        fplt.make_addplot(
-            uptrend,
-            color="green",
-            linestyle='dotted',
-            # type='scatter',
-            width=2,
-            alpha=0.8,
-            marker=".",
-            # markersize=20,
-        ),
-        fplt.make_addplot(
-            downtrend,
-            color="red",
-            linestyle='dotted',
-            # type='scatter',
-            width=2,
-            alpha=0.8,
-            marker=".",
-            # markersize=20,
-        ),
-    ]
+def add_ohcl_chart(fig, candles: CandleLike):
+    fig.add_ohlc(
+        x=candles.index,
+        open=candles.open,
+        high=candles.high,
+        low=candles.low,
+        close=candles.close,
+        name="price"
+    )
 
-    fplt.plot(
-        df_rates.rename(columns=mpl_comp_names),
-        type="ohlc",  # 'hollow_and_filled',  # 'candle',
-        style='yahoo',  # 'charles',
-        ylabel='Price ($GBP)',
-        title=symbol,
-        volume=True,
-        tight_layout=True,
-        figscale=1.25,
-        figratio=(5, 2),
-        # mav=(5, 20),
-        savefig=saving_config,
-        addplot=trendmarkers,
-        fill_between=[
-            {
-                "y1": df_rates['down'].values,
-                "y2": df_rates['up'].values,
-                # "where": df_rates["signal"] == 1,
-                "color": "gray",
-                "alpha": 0.07,
-                # "interpolate": True,
-                # "step": 'pre',
-                "antialiased": True
-            },
-        ]
+    fig.update_yaxes(
+        side="right",
+        tickprefix="$",
+        ticklabelposition="outside right",
+    )
+
+    fig.update_layout(
+        xaxis_rangeslider_visible=False,
+        legend=dict(
+            groupclick="toggleitem",
+            orientation="h",
+            # xanchor="left",
+            # yanchor="top",
+            y=1.25,
+            x=0,
+        )
+    )
+
+
+def add_trending_line(
+    fig: go.Figure,
+    candles: CandleLike,
+    source1: str,
+    source2: str,
+    band: tuple[float, float] = (0, 0),
+    legendgroup: str = "regime",
+):
+    # Full line
+    series1 = candles[source1]
+    series2 = candles[source2]
+
+    bulls = np.where(above(series2, series1, band[1]), series1, np.nan)
+    fig.add_scattergl(
+        x=candles.index,
+        y=bulls,
+        name="bullish",
+        mode="markers+lines",
+        line={'color': 'green', 'width': 1},
+        marker={"color": "green", "size": 3},
+        legendgroup=legendgroup,
+        legendgrouptitle_text=legendgroup,
+    )
+
+    neutrals = np.where(onband(series2, series1, band), series1, np.nan)
+    fig.add_scattergl(
+        x=candles.index,
+        y=neutrals,
+        name="neutral",
+        mode="markers+lines",
+        line={'color': 'grey', 'width': 1},
+        marker={"color": "grey", "size": 3},
+        legendgroup=legendgroup,
+    )
+
+    # Above threshold
+    bears = np.where(below(series2, series1, band[0]), series1, np.nan)
+    fig.add_scattergl(
+        x=candles.index,
+        y=bears,
+        name="bearish",
+        mode="markers+lines",
+        line={'color': 'red', 'width': 1},
+        marker={"color": "red", "size": 3},
+        legendgroup=legendgroup,
+    )
+
+    #fig.add_scatter(x=candles.index, y=series2)
+
+
+def add_lines(
+    fig: go.Figure,
+    candles: CandleLike,
+    sources: tuple[str],
+    legendgroup: str = "indicator",
+    ylim: tuple[float] = (0, 1)
+):
+    for source in sources:
+        series = candles[source]
+
+        fig.add_scattergl(
+            x=candles.index,
+            y=series,
+            name=source,
+            mode="lines",
+            line={'color': 'red', 'width': 1},
+            legendgroup=legendgroup,
+            legendgrouptitle_text=legendgroup,
+        )
+
+    fig.update_yaxes(
+        side="right",
+        ticklabelposition="outside right",
+        # fixedrange=True,
+        range=ylim,
+    )
+
+    fig.update_layout(
+        # xaxis_rangeslider_visible=False,
+        legend=dict(
+            groupclick="toggleitem",
+            orientation="h",
+            # xanchor="left",
+            # yanchor="top",
+            y=1.25,
+            x=0,
+        )
     )
