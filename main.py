fd7ffe8cb3404f74f2877d6c58d73b40c46e134c
KevinEsh <kevin_esh@outlook.com>
2023-06-07 16:52:28 -0600

    main

--- main.py
+++ main.py
@@ -2,13 +2,16 @@ from time import sleep
 from numpy import ones
 from datetime import datetime as dt
 
+today = dt.now().strftime('%Y-%M-%d')
+
+
 print("Hello world")
 print(ones(10))
 
-with open(r'C:/Users/grung/Desktop/kpltrader/session.log', "w") as file:
+with open(f'logs/{today}.log', "w") as file:
     print("writting")
     file.write(dt.now().isoformat())
 
 sleep(5)
 
-# C: \Users\grung\Desktop\kpltrader\traderbot.py
+# C: \Users\grung\Desktop\kpltrader\traderbot.py >=
