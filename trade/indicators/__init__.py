9329559141b1ccde7c57f8c79609f201686dcbcb
KevinEsh <kevin_esh@outlook.com>
2023-06-07 16:55:21 -0600

    oc2 and heikinashi indicators included

--- trade/indicators/__init__.py
+++ trade/indicators/__init__.py
@@ -1,7 +1,7 @@
 from talib import EMA, SMA, CCI, ADX, RSI, MAX, MIN, ATR, STDDEV, VAR
 from talib.stream import SMA as SMA_, ATR as ATR_, STDDEV as STDDEV_, VAR as VAR_
 from trade.indicators.custom import HL2, HLC3, OHLC4, PIVOTHIGH, PIVOTLOW, \
-    DONCHAIN, WT, RQK, RBFK
+    DONCHAIN, WT, RQK, RBFK, HEIKINASHI
 
 from talib import set_unstable_period, get_unstable_period
 from trade.indicators.basic import get_all_indicators, get_min_bars, get_stable_min_bars
