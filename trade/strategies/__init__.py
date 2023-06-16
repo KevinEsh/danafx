f75566243222ae4104c76e87f3a0d72c16a72008
KevinEsh <kevin_esh@outlook.com>
2023-06-15 23:28:24 -0600

    fix on compound strategy

--- trade/strategies/__init__.py
+++ trade/strategies/__init__.py
@@ -1,3 +1,4 @@
 from trade.strategies.trending.dualsma import DualSmaStrategy
 from trade.strategies.trending.nadaraya_watson import DualNadarayaKernelStrategy
 from trade.strategies.scalping.minmax import MinMaxStrategy
+from trade.strategies.abstract import CompoundTradingStrategy, Priority, And, Or
