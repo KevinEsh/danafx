d3e3a4295a183c9d61612c0c1af55970daacff7d
KevinEsh <kevin_esh@outlook.com>
2023-06-22 01:27:24 -0600

    WIP: match_entries

--- trade/strategies/exit/dirchange.py
+++ trade/strategies/exit/dirchange.py
@@ -5,27 +5,85 @@ from trade.strategies.abstract import ExitTradingStrategy, Hyperparameter
 
 from datatools.custom import get_recarray, shift, find_supreme
 
+def get_exit_indexes(candles, lag, length):
+    if lag == 0:
+        candle_buy_breaks = np.abs(candles.low - candles.open) > length
+        candle_sell_breaks = np.abs(candles.high - candles.open) > length
+
+        buy_exit_indexes = np.where(candle_buy_breaks)[0]
+        sell_exit_indexes = np.where(candle_sell_breaks)[0]
+    else:
+        candle_directions = candles.close > candles.open
+        candle_breaks = np.abs(candles.close - candles.open) > length
+
+        buy_exit_indexes = np.where(shift(~candle_directions & candle_breaks, lag, False))[0]
+        sell_exit_indexes = np.where(shift(candle_directions & candle_breaks, lag, False))[0]
+
+    return get_recarray([buy_exit_indexes, sell_exit_indexes], names=['buy', 'sell'])
+
+def get_entry_indexes(entry_signals, as_prices: bool):
+    if not as_prices:
+        buy_entry_indexes = np.where(entry_signals.buy)[0]
+        sell_entry_indexes = np.where(entry_signals.sell)[0]
+    else:
+        buy_entry_indexes = np.where(~np.isnan(entry_signals.buy))[0]
+        sell_entry_indexes = np.where(~np.isnan(entry_signals.sell))[0]
+
+    return get_recarray([buy_entry_indexes, sell_entry_indexes], names=['buy', 'sell'])
+
+def match_entries_to_exits(
+    entry_indexes, 
+    exit_indexes, 
+    opens, 
+    lag: int, 
+    length: float,
+    as_prices: bool, 
+) -> np.recarray:
+    # Create new arrays to store exit information
+    n = opens.shape[0]
+    buy_exit_signals = np.full(n, np.NaN)
+    sell_exit_signals = np.full(n, np.NaN)
+
+    if as_prices:
+        exit_prices = np.full(n, np.NaN)
+
+    for b_entry in entry_indexes.buy:
+
+        b_exit = find_supreme(exit_indexes.buy, b_entry + lag)
+        b_exit = n - 1 if (b_exit is None) else b_exit #TODO: poner un parametro para que el algo siempre cierre todas las operacion con la ultima vela
+        buy_exit_signals[b_entry] = b_exit
+        if as_prices:
+            exit_prices[b_entry] = opens[b_exit] if lag else opens[b_exit] - length
+
+    if not as_prices:
+        return get_recarray([buy_exit_signals, sell_exit_signals], names=["buy", "sell"])
+    else:
+        return get_recarray([
+            buy_exit_signals, sell_exit_signals, 
+            sell_exits[0], sell_exits[1]], 
+            names=["buy", "buy_price", "sell", "sell_price"])
+
 class DirectionChangeExitStrategy(ExitTradingStrategy):
     # config_window = Hyperparameter("window", "numeric", (2, 1000))
-    config_neutral_length = Hyperparameter("neutral_length", "numeric", (0, 10))
+    config_length = Hyperparameter("length", "numeric", (0, 10))
     config_lag = Hyperparameter("lag", "numeric", (0, 3))
     config_only_profit = Hyperparameter("only_profit", "boolean")
 
     def __init__(
         self,
         # window: int = 14,
-        neutral_length: float = 0,
+        length: float = 0,
         lag: int = 1,
         only_profit: bool = False,
     ) -> None:
         super().__init__()
         # self.config_window._check_bounds(window, init=True)
-        self.config_neutral_length._check_bounds(neutral_length, init=True)
+        self.config_length._check_bounds(length, init=True)
         self.config_lag._check_bounds(lag, init=True)
         self.config_only_profit._check_bounds(only_profit, init=True)
 
         # self._window = window
-        self._neutral_length = neutral_length
+        self._length = length
         self._lag = lag
         self._only_profit = only_profit
 
@@ -41,13 +99,13 @@ class DirectionChangeExitStrategy(ExitTradingStrategy):
     #     self._window = window
 
     @property
-    def neutral_length(self):
-        return self._neutral_length
+    def length(self):
+        return self._length
 
-    @neutral_length.setter
-    def neutral_length(self, neutral_length):
-        self.config_neutral_length._check_bounds(neutral_length)
-        self._neutral_length = neutral_length
+    @length.setter
+    def length(self, length):
+        self.config_length._check_bounds(length)
+        self._length = length
 
     @property
     def only_profit(self):
@@ -92,7 +150,7 @@ class DirectionChangeExitStrategy(ExitTradingStrategy):
         # print(candle_direction, position_direction, candle.close, position.price_open, candle_length)
         
         # Generate an exit signal if the directions are opposite and exceeds the threshold
-        if (position_direction != candle_direction) and (candle_length > self.neutral_length):
+        if (position_direction != candle_direction) and (candle_length > self.length):
             return ExitSignal.EXIT #ExitSignal.SELL if position_direction else ExitSignal.BUY
         else:
             return ExitSignal.HOLD
@@ -107,54 +165,21 @@ class DirectionChangeExitStrategy(ExitTradingStrategy):
 
         n = entry_signals.shape[0]
         m = self.train_data.shape[0]
-
-        if entry_signals.shape[0] != self.train_data.shape[0]:
+        if n != m:
             raise ValueError(f"entry_signals lenght must be {m} but received {n}")
 
         opens = self.train_data.open
+        entry_indexes = get_entry_indexes(entry_signals, as_prices)
+        exit_indexes = get_exit_indexes(self.train_data, self._lag, self._length)
 
-        if not as_prices:
-            buy_entry_indexes = np.where(entry_signals.buy)[0]
-            sell_entry_indexes = np.where(entry_signals.sell)[0]
-        else:
-            buy_entry_indexes = np.where(~np.isnan(entry_signals.buy))[0]
-            sell_entry_indexes = np.where(~np.isnan(entry_signals.sell))[0]
+        return match_entries_to_exits(entry_indexes, exit_indexes, opens, self._lag, self._length, as_prices)
+        # if self._lag == 0:
+        #     buy_exit_prices = np.where(buy_exit_signals, opens - self.length, np.NaN)
+        #     sell_exit_prices = np.where(sell_exit_signals, opens + self.length, np.NaN)
+        # else:
+        #     buy_exit_prices = np.where(buy_exit_signals, opens, np.NaN)
+        #     sell_exit_prices = np.where(sell_exit_signals, opens, np.NaN)
 
-        if self._lag == 0:
-            candle_buy_breaks = np.abs(self.train_data.low - opens) > self.neutral_length
-            candle_sell_breaks = np.abs(self.train_data.high - opens) > self.neutral_length
-
-            buy_exit_indexes = np.where(candle_buy_breaks)[0]
-            sell_exit_indexes = np.where(candle_sell_breaks)[0]
-        else:
-            candle_directions = self.train_data.close > opens
-            candle_breaks = np.abs(self.train_data.close - opens) > self.neutral_length
-        
-            buy_exit_indexes = np.where(shift(~candle_directions & candle_breaks, self.lag, False))[0]
-            sell_exit_indexes = np.where(shift(candle_directions & candle_breaks, self.lag, False))[0]
-
-        buy_exit_signals = np.zeros(n, dtype=bool)
-        sell_exit_signals = np.zeros(n, dtype=bool)
-
-        for b_entry in buy_entry_indexes:
-            b_exit = find_supreme(buy_exit_indexes, b_entry + self._lag)
-            if b_exit is not None:
-                buy_exit_signals[b_exit] = True
-
-        for s_entry in sell_entry_indexes:
-            s_exit = find_supreme(sell_exit_indexes, s_entry + self._lag)
-            if s_exit is not None:
-                sell_exit_signals[s_exit] = True
-
-        if not as_prices:
-            return get_recarray([buy_exit_signals, sell_exit_signals], names=["buy", "sell"])
-
-        if self._lag == 0:
-            buy_exit_prices = np.where(buy_exit_signals, opens - self.neutral_length, np.NaN)
-            sell_exit_prices = np.where(sell_exit_signals, opens + self.neutral_length, np.NaN)
-        else:
-            buy_exit_prices = np.where(buy_exit_signals, opens, np.NaN)
-            sell_exit_prices = np.where(sell_exit_signals, opens, np.NaN)
 
 
         # if self._only_profit:
